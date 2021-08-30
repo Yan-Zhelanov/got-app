8337e95002589e273a4762d09d7f95f2e4e3a58c
Yan-Zhelanov <yzhelanov@gmail.com>
2021-08-30 21:52:37 +0300

    v1.0 — Updated on the main page welcome message

--- src/components/pages/main.js
+++ src/components/pages/main.js
@@ -8,7 +8,8 @@ export default class MainPage extends React.Component {
     return (
       <div className="main">
         <h3 className="main-header">Welcome to GoT site!</h3>
-        <p className="main-text">This is a small one-page application dedicated to Game of Thrones. Thank you very much anapioficeandfire.com for the free API.</p>
+        <p className="main-text">This is a small one-page application dedicated to Game of Thrones.</p>
+        <p className="additional-text">Thank you very much anapioficeandfire.com for the free API.</p>
       </div>
     );
   }
